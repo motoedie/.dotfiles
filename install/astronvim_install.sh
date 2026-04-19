c30849f4c7e6109974857c1a65fc43a04a448651
Ladislav Starek <ladislav.starek@gmail.com>
2026-04-19 01:17:38 +0000

    Clone astronvim config using ssh

--- install/astronvim_install.sh
+++ install/astronvim_install.sh
@@ -1,3 +1,3 @@
 echo "\n\033[0;33mIntalling Astronvim\033[0m"
 rm -rf ~/.config/nvim
-git clone https://github.com/motoedie/astronvimv5 ~/.config/nvim
+git clone git@github.com:motoedie/astronvimv5.git ~/.config/nvim
